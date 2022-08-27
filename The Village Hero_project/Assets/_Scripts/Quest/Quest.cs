bc7ca1b5d55913d40e36d5e5feaf7b40fa10d9c9
FunGi2410 <110833435+FunGi2410@users.noreply.github.com>
2022-08-27 17:54:50 +0700

    add Quest class

---
+++ The Village Hero_project/Assets/_Scripts/Quest/Quest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quest
+{
+    public enum QuestProgress
+    {
+        NOT_AVAILABLE,
+        AVAILABLE,
+        ACCEPTED,
+        COMPLETE,
+        DONE
+    }
+
+    public string title;
+    public int id;
+    public QuestProgress progress;
+    public string description;
+    public string hint;
+    public string congratulation;
+    public string sumary;
+    public int nextQuest;
+
+    public string questObjective;
+    public int questObjectiveCount;
+    public int questObjectiveRequirement;
+
+    // Reward
+}
